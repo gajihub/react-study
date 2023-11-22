1022bd03ab5ad87d37504444fbe98b46fcfb4bac
nana <nana@gmail.com>
2023-11-22 13:53:38 +0900

    [추가] mail table list 추가

--- src/pages/Container.tsx
+++ src/pages/Container.tsx
@@ -1,17 +1,19 @@
 import {
-  OutlinedInput,
-  FormControl,
+  TextField,
   InputAdornment,
   Checkbox,
-  TableSortLabel,
   Table,
   TableBody,
   TableCell,
   TableContainer,
-  TableHead,
   TableRow
 } from "@mui/material";
-import { Search } from "@mui/icons-material";
+import {
+  Search,
+  StarOutline,
+  Email,
+  DraftsOutlined
+} from "@mui/icons-material";
 import styles from "@styles/pages/Container.module.scss";
 
 const Container = () => {
@@ -19,19 +21,23 @@ const Container = () => {
     <>
       <div className={styles.container}>
         <div className={styles.searchBox}>
-          <FormControl sx={{ m: 1 }} variant="standard">
-            <OutlinedInput
-              endAdornment={
+          <TextField
+            label="검색"
+            InputProps={{
+              startAdornment: (
                 <InputAdornment position="end">
                   <Search />
                 </InputAdornment>
-              }
-            />
-          </FormControl>
+              )
+            }}
+          />
         </div>
         <div>
           <TableContainer sx={{ maxHeight: 440 }}>
-            <Table stickyHeader aria-label="sticky table">
+            <Table
+              stickyHeader
+              aria-label="sticky table"
+              className={styles.mailListTable}>
               <TableBody>
                 <TableRow hover sx={{ cursor: "pointer" }}>
                   <TableCell padding="checkbox">
@@ -42,13 +48,80 @@ const Container = () => {
                       }}
                     />
                   </TableCell>
-                  <TableCell component="th" scope="row" padding="none">
-                    name
+                  <TableCell
+                    component="th"
+                    scope="row"
+                    padding="none"
+                    colSpan={5}>
+                    <ul className={styles.tableHead}>
+                      <li>읽음</li>
+                      <li>삭제</li>
+                      <li>스팸차단</li>
+                      <li>답장</li>
+                      <li>전체답장</li>
+                      <li>전달</li>
+                      <li>이동</li>
+                      <li>필터</li>
+                      <li>안읽음 삭제 또는 1개 선택</li>
+                    </ul>
                   </TableCell>
-                  <TableCell align="right">calories</TableCell>
-                  <TableCell align="right">fat</TableCell>
-                  <TableCell align="right">carbs</TableCell>
-                  <TableCell align="right">protein</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell padding="none">
+                    <Checkbox
+                      color="primary"
+                      inputProps={{
+                        "aria-labelledby": ""
+                      }}
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <StarOutline />
+                  </TableCell>
+                  <TableCell>
+                    <Email />
+                  </TableCell>
+                  <TableCell>보낸사람</TableCell>
+                  <TableCell>[카테고리] 제목 [새창으로보기]</TableCell>
+                  <TableCell>받은시간</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell padding="none">
+                    <Checkbox
+                      color="primary"
+                      inputProps={{
+                        "aria-labelledby": ""
+                      }}
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <StarOutline />
+                  </TableCell>
+                  <TableCell>
+                    <Email />
+                  </TableCell>
+                  <TableCell>보낸사람</TableCell>
+                  <TableCell>[카테고리] 제목 [새창으로보기]</TableCell>
+                  <TableCell>받은시간</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell padding="none">
+                    <Checkbox
+                      color="primary"
+                      inputProps={{
+                        "aria-labelledby": ""
+                      }}
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <StarOutline />
+                  </TableCell>
+                  <TableCell>
+                    <DraftsOutlined />
+                  </TableCell>
+                  <TableCell>보낸사람</TableCell>
+                  <TableCell>[카테고리] 제목 [새창으로보기]</TableCell>
+                  <TableCell>받은시간</TableCell>
                 </TableRow>
               </TableBody>
             </Table>
